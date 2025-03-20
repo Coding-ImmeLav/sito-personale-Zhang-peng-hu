c92492f09be6fb63689513872f202be3b631cc61
Zhang-peng-hu <zhang.hu@edu.immaginazioneelavoro.it>
2025-03-20 11:17:09 +0100

    sito

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = 0;
+function compariMenu() {
+    if(a==0) {
+        document.getElementById("menu").style.display = "inital";
+        a = 1;
+    }
+    else if(a==1) {
+        document.getElementById("menu").style.display = "none";
+        a = 0;
+    }
+}
