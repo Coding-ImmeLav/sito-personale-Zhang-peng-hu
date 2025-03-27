61ae5c6b13a300e2e9f658920157202321c96167
Zhang-peng-hu <zhang.hu@edu.immaginazioneelavoro.it>
2025-03-27 10:54:29 +0100

    sito

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let a = 0;
+
+let a = 0; 
 function compariMenu() {
-    if(a==0) {
-        document.getElementById("menu").style.display = "inital";
+    if(a == 0) {
+        document.getElementById("menu").style.display = "initial";
         a = 1;
     }
-    else if(a==1) {
+    else if(a == 1) {
         document.getElementById("menu").style.display = "none";
         a = 0;
     }
 }
+
